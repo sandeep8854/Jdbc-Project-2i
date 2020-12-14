d2117f5e910b872444e3938b8357fa747d228a39
sandeep8854 <sandeepstructure@gmail.com>
2020-12-14 20:17:33 +0530

    BatchUpdateSimpleStatement...

--- Jdbc-Project-2i/src/sandeep/algoExpert/com/BatchUpdateSimpleStatement.java
+++ Jdbc-Project-2i/src/sandeep/algoExpert/com/BatchUpdateSimpleStatement.java
@@ -9,19 +9,22 @@ public class BatchUpdateSimpleStatement
 	public static void main(String[] args) throws Exception 
 	{
 		String Driver="com.mysql.jdbc.Driver";
-		String jdbc_url="jdbc:mysql://localhost:3306/movies"; 
+		String jdbc_url="jdbc:mysql://localhost:3306/employees"; 
 		String user="root";
 		String pwd="root"; 
 		Class.forName(Driver);
-		Connection con=DriverManager.getConnection("jdbc_url", "user", "pwd");
+		Connection con=DriverManager.getConnection(jdbc_url, user, pwd);
 		Statement st=con.createStatement();
-		
-		
-		
-		
-		
-		
+		st.addBatch("insert into employees values(700,'Rahul',80000,'Gazipur')");
+		st.addBatch("update employees set esal=30000 where EmpNo=300");
+		st.addBatch("delete from employees where EmpNo=500");
+		int[] count=st.executeBatch();
+		int updateCount=0;
+		for(int x :count)
+		{
+			updateCount=updateCount+x;
+		}
+		System.out.println("Number Of Row Updated "+updateCount);
+		con.close();
 	}
-	
-
 }
